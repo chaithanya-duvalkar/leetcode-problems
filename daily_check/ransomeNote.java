c9b94763ab656e9d05d9d04387a362b84a7b9227
Chaithanya S <chaithanya9ig@gmail.com>
2025-10-23 00:09:33 +0530

    ransomNote hashmap code added

--- daily_check/ransomeNote.java
+++ daily_check/ransomeNote.java
@@ -20,6 +20,27 @@ public class ransomeNote {
         }
         return true;
 
+        // Map<Character,Integer> map=new HashMap<>();
+
+        // for(char c:magazine.toCharArray())
+        // {
+        //     map.put(c,map.getOrDefault(c,0)+1);
+        // }
+        // for(char c:ransomNote.toCharArray())
+        // {
+        //     if(!map.containsKey(c) || map.get(c)==0)
+        //     {
+        //         return false;
+        //     }
+        //     else
+        //     {
+        //         map.put(c,map.get(c)-1);
+        //     }
+        // }
+        // return true;
+
+
+
 
         
     }
